fdb75bec6a768b9024d958d08f89752a5d0a2eb9
Eugene Bekker <ebekker@users.noreply.github.com>
2018-02-23 16:03:18 -0500

    Adding base request handler implementations

---
+++ src/Zyborg.Vault.MockServer/Routing/BaseRequestHandler.cs
@@ -0,0 +1,56 @@
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Routing;
+using Zyborg.Vault.MockServer.Routing.Results;
+
+namespace Zyborg.Vault.MockServer.Routing
+{
+    public abstract class BaseRequestHandler : IRequestHandler
+    {
+        public const string HttpGetMethod = "GET";
+        public const string HttpListMethod = "LIST";
+        public const string HttpPutMethod = "PUT";
+        public const string HttpPostMethod = "POST";
+        public const string HttpDeleteMethod = "DELETE";
+        public const string QueryListParameterKey = "list";
+
+        public async Task<IHandlerResult> HandleAsync(HttpContext http)
+        {
+            var requ = http.Request;
+            var childPath = http.GetRouteValue("path") as string;
+
+            switch (requ.Method)
+            {
+                case HttpGetMethod:
+                    if (requ.Query.ContainsKey("list"))
+                        return await HandleListAsync(http, childPath);
+                    else
+                        return await HandleGetAsync(http, childPath);
+                
+                case HttpListMethod:
+                    return await HandleListAsync(http, childPath);
+                
+                case HttpPutMethod:
+                    return await HandlePutAsync(http, childPath);
+                
+                case HttpPostMethod:
+                    return await HandlePostAsync(http, childPath);
+                
+                case HttpDeleteMethod:
+                    return await HandleDeleteAsync(http, childPath);
+            }
+
+            return await Task.FromResult(StatusCodeResult.MethodNotAllowedResult);
+        }
+
+        public abstract Task<IHandlerResult> HandleListAsync(HttpContext http, string childPath);
+
+        public abstract Task<IHandlerResult> HandleGetAsync(HttpContext http, string childPath);
+
+        public abstract Task<IHandlerResult> HandlePutAsync(HttpContext http, string childPath);
+
+        public abstract Task<IHandlerResult> HandlePostAsync(HttpContext http, string childPath);
+
+        public abstract Task<IHandlerResult> HandleDeleteAsync(HttpContext http, string childPath);
+    }
+}
---
+++ src/Zyborg.Vault.MockServer/Routing/BaseRequestHandlerOf.cs
@@ -0,0 +1,44 @@
+using System;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Zyborg.Vault.MockServer.Routing
+{
+    public abstract class BaseRequestHandler<TObject> : BaseRequestHandler
+    {
+
+        public abstract Task<IHandlerResult> HandleDeleteAsync(TObject obj, HttpContext http,
+            string childPath);
+
+        public abstract Task<IHandlerResult> HandleGetAsync(TObject obj, HttpContext http,
+            string childPath);
+
+        public abstract Task<IHandlerResult> HandleListAsync(TObject obj, HttpContext http,
+            string childPath);
+
+        public abstract Task<IHandlerResult> HandlePostAsync(TObject obj, HttpContext http,
+            string childPath);
+
+        public abstract Task<IHandlerResult> HandlePutAsync(TObject obj, HttpContext http,
+            string childPath);
+
+        public virtual TObject CreateInstance(HttpContext http) => 
+            Activator.CreateInstance<TObject>();
+
+        public override Task<IHandlerResult> HandleDeleteAsync(HttpContext http, string childPath) =>
+                HandleDeleteAsync(CreateInstance(http), http, childPath);
+
+        public override Task<IHandlerResult> HandleGetAsync(HttpContext http, string childPath) =>
+                HandleDeleteAsync(CreateInstance(http), http, childPath);
+
+        public override Task<IHandlerResult> HandleListAsync(HttpContext http, string childPath) =>
+                HandleDeleteAsync(CreateInstance(http), http, childPath);
+
+        public override Task<IHandlerResult> HandlePostAsync(HttpContext http, string childPath) =>
+                HandleDeleteAsync(CreateInstance(http), http, childPath);
+
+        public override Task<IHandlerResult> HandlePutAsync(HttpContext http, string childPath) =>
+                HandleDeleteAsync(CreateInstance(http), http, childPath);
+    }
+}
